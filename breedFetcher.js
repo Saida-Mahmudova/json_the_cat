c79d97a1b6185e501bb503999cd573f0367cd96c
Saida-Mahmudova <mahmudovas.au@gmail.com>
2022-03-18 12:43:58 +0300

    refactoring of previous code and adding mocha tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,19 @@
 const request = require('request');
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log("Error occured:", error);
-  }
-  const data = JSON.parse(body);
-  const breedBody = data[0];
-  if (breedBody) {
-    console.log(data[0].description);
-  } else {
-    console.log("Breed is not found");
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log("Error occured:", error);
+    }
+    const data = JSON.parse(body);
+    const breedBody = data[0];
+    if (breedBody) {
+      callback(null, data[0].description);
+    } else {
+      callback(error, null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
